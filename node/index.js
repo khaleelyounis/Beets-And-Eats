bff561ccdc43575e3e1c63bbbb7cdab0616fb901
Khaleel Younis <khaleelyounis1@gmail.com>
2018-03-23 23:45:23 -0700

    fixing routing and file path

--- node/index.js
+++ node/index.js
@@ -20,11 +20,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 //Path middleware
-app.use(express.static(path.join(__dirname, "..", "client")));
-
-app.get('*', (req, res) => {
-    res.send(express.static(path.join(__dirname, "..", "client", "index.html")));
-});
+app.use('/', express.static(path.join(__dirname, '../client/index.html')));
 
 app.get('/yelprequest', (req, res) => {
     console.log('req.query: ', req.query);
