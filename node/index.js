c3f9c31a63c239b33bfb94f1a038e9a2058439f2
Khaleel Younis <khaleelyounis1@gmail.com>
2018-03-23 23:03:12 -0700

    fixed node routing

--- node/index.js
+++ node/index.js
@@ -20,8 +20,11 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 //Path middleware
-app.get("/", (req, res) => {
-    res.send(express.static(path.join(__dirname, "client", "index.html")));
+app.use(express.static(path.join(__dirname, '..', "client", "index.html")));
+
+//Route for all static files from the client side
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'client', 'index.html'));
 });
 
 app.get('/yelprequest', (req, res) => {
