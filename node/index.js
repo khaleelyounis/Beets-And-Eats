808dc2b1cd9f71de330badf3e5bc90d392c70898
Khaleel Younis <khaleelyounis1@gmail.com>
2018-03-23 17:41:13 -0700

    changed the route once more

--- node/index.js
+++ node/index.js
@@ -20,7 +20,9 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 //Path middleware
-app.use(express.static(path.join(__dirname, '..', 'index.html')));
+app.get("/", (req, res) => {
+    app.use(express.static(path.join(__dirname, '..', 'index.html')));
+});
 
 app.get('/yelprequest', (req, res) => {
     console.log('req.query: ', req.query);
