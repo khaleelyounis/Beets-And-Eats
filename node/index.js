371592c26d9f62e8678710ebfa146a79a23e3d50
Khaleel Younis <khaleelyounis1@gmail.com>
2018-03-23 23:29:24 -0700

    fixing routing

--- node/index.js
+++ node/index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 //Path middleware
-app.use(express.static(path.join(__dirname, '..', 'client')));
+app.use('/', express.static(path.join(__dirname, '../client/index.html')));
 
 app.get('/yelprequest', (req, res) => {
     console.log('req.query: ', req.query);
